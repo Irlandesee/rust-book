0dd5732499cf304b5628ef5488aee563e7479726
Mattia Lunardi <mattiamauro.lunardi@elmec.it>
2025-08-30 21:39:44 +0200

    feat: complete ownershipt

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -61,5 +61,80 @@ fn main() {
     } //This scope is no longer valid, and is no longer valid
     //When a variable goes out of scope, rust calls a special function called drop. (RAII pattern)
 
-    // Variables and DAta interacting with move
-}
+    // Variables and Data interacting with move
+    let x1 = 5;
+    let y1 = x1; //Bind the value 5 to x, then make a copy of the value in x and bind it to y
+    //
+    // A String is made up of three parts, a pointer to the memory that holds the contents of the
+    // string, a length, and a capacity. This group of data is stored on the stack.
+    // The lenght is how much memory, in bytes, the contents of the String are currently using.
+    // The capacity is the total amount of memory, in bytes, that the String has received from the
+    // allocator.
+    //
+    let s1 = String::from("Hello");
+    let s2 = s1; //When s1 is assigned to s2, the String data is copied, meaning the pointer, the length,
+    //and the capacity that are on the stack. The data on the heapthat the pointer refers to is not copied.
+
+    //When a varible goes out of scope, rust automatically calls the drop function and cleans up
+    //the heap memory for that variable: This is a problem, because s2 and s1 go out of scope they
+    //will both try to free the same memory. This is known as a doable free error: Freeing memory
+    //twice can lead to memory corruption, which can potentially lead to security vulnerabilities.
+    //
+    //To ensure memory safety, after the line
+    //let s2 = s1;
+    //Rust consides s1 no longer valid. Therefore, Rust does not need to free anything when s1
+    //goes out of scope.
+    //
+    //This concept is called move: s1 was moved to s2
+    //Rust will never automatically create deep copies of the data, any automatic copying can be
+    //assumed to be inexpensive in terms of runtime performance.
+    //
+    //Scope and Assignment
+    //The inverse of this is true for the relationship between scoping, ownership and memory being
+    //freed via the drop function as well: when an existing variable is assigned to a completely
+    //new variable, Rust will call drop and free the original value's memory immediately
+    let mut s2 = String::from("Hello");
+    s2 = String::from("ahoy"); //At this point, nothing is referring to the original value on the
+    //heap at all
+    //Thus the original string goes immediately out of scope.
+    println!("{s2}, world"); //Ahoy World is printed
+    //
+    //Variables and data interacting with clone
+    //When a deep copy of the heap data is needed, a method called clone can be used.
+    //
+    let u = String::from("hello");
+    let u2 = u.clone(); //The heap data is actually copied
+    //
+    // Stack only data: Copy
+    // Types such as integers have a known size at compile time are stored entirely on the stack,
+    // so copies of the actual values are quick to make. There is no difference between deep and
+    // shallow copying here, so calling clone wouldn't do anything different from the usual shallow
+    // copying.
+    // If a type implements the Copy trait, variables that use it do not move, but rather are
+    // trivially copied, making them still valid after assignment to another value
+    //
+    // Rust won't let us annotate a type with Copy if the type, or any of its parts, has
+    // implemented the Drop trait. Nothing that requires allocation or is some form of resource can
+    // implement Copy.
+    //
+    // Ownership and function
+    // The mechanics of passing a value to a function are similar to those when assigning a value
+    // to a variable. Passing a variable to a function will move or copy, just as assignment does.
+    //
+    let s3 = String::from("HelloWorld!");
+    takes_ownership(s3); // s's value moves into the function, and is no longer valid here
+
+    let x3 = 5; //x comes into scope
+    makes_copy(x3); //i32 implements the Copy trait, x3 does NOT move into the function, its okay
+    //to use x afterward
+} // x3 goes out of scope, then s. S's value was moved, nothing special happens
+
+fn takes_ownership(some_string: String) {
+    //some_string comes into scope
+    println!("{some_string}");
+} //some_string goes out of scope and the drop is called. The backing memory is freed
+
+fn makes_copy(some_integer: i32) {
+    //some_integer comes into scope
+    println!("{some_integer}");
+} // Some integer goes out of scope, nothing special happnes
